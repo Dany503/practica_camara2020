6864764499f4459eaf29006196a3e2c3c6ce7504
Daniel Gutiérrez <d.gutierrez.reina@gmail.com>
2020-10-29 11:55:52 +0100

    Update ejercicio_10.py

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -14,7 +14,7 @@ rawCapture = PiRGBArray(camera, size=(320, 240))
 
 # allow the camera to warmup
 time.sleep(0.1)
-print 'Saca la primera foto pulsando A'
+print("Saca la primera foto pulsando a")
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# grab the raw NumPy array representing the image, then initialize the timestamp
@@ -26,24 +26,23 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     key = cv2.waitKey(1) & 0xFF
     rawCapture.truncate(0)
  	# if the `q` key was pressed, break from the loop
-    if key == ord("A"):
+    if key == ord("a"):
          #cv2.imwrite("afoto.jpg",image)
          imagen1=image
          cv2.imshow("A",imagen1)
          time.sleep(3)
 
-         print 'Saca la segunda foto pulsando B'
-         #break
-    if key == ord("B"):
+         print("Saca la segunda foto pulsando B")
+    if key == ord("b"):
          #cv2.imwrite("afoto.jpg",image)
          imagen2=image
          cv2.imshow("B",imagen2)
          time.sleep(3)
 
-         print 'Para mezclarlas, pulsa M'
+         print("Para mezclarlas, pulsa M")
          #break
-    if key == ord("M"):
-         alfa=raw_input("mete porcentaje de mezcla")
+    if key == ord("m"):
+         alfa=input("mete porcentaje de mezcla")
          alfa_f=float(alfa)/100.0
          beta=1.0-alfa_f
          dst = cv2.addWeighted(imagen1,alfa_f,imagen2,beta,0)
@@ -51,7 +50,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
          print "Mezcla al %s" %alfa
            
          time.sleep(3)
-         print 'Para salir, pulsa q'
+         print("Para salir, pulsa q")
     if key == ord("q"):
         break
          
