78b43e16b602bac79e7919534514b5070b796f06
Daniel Gutiérrez <d.gutierrez.reina@gmail.com>
2020-10-29 11:56:30 +0100

    Update ejercicio_3.py

--- ejercicio_3.py
+++ ejercicio_3.py
@@ -6,7 +6,7 @@ camera.resolution = (640,480)
 camera.rotation = 180
 camera.start_preview(fullscreen=False, window=(30,30,320,240))
 for i in range(1,4):
-    print 4-i
+    print(4-i)
     sleep(1)
 camera.capture('/home/pi/imagen.jpg')
 camera.stop_preview()
