54550a3ea22890a24423ee161c2fb4d86f5ec85c
Daniel Gutiérrez <d.gutierrez.reina@gmail.com>
2020-10-29 11:57:13 +0100

    Update ejercicio_7.py

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -28,7 +28,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
  
 	# if the `q` key was pressed, break from the loop
     if key == ord("q"):
-         cv2.imwrite("afoto.jpg",image)
+         cv2.imwrite("/home/pi/foto.jpg",image)
          break
 camera.close()
 cv2.destroyWindow("Frame")
